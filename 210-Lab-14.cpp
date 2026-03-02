0e0d157f902b94ba967a44b6e6d038e057ab0cf6
JoaquinF <joaquinpfelix@gmail.com>
2026-03-01 16:47:59 -0800

    Check 2

--- 210-Lab-14.cpp
+++ 210-Lab-14.cpp
@@ -41,3 +41,69 @@ void populateColor(Color &c, int r, int g, int b)
     c.setGreen();
     c.setBlue();
 }
+
+void Color::setRed(int r) 
+{
+    if (red >= MIN_RGB && red <= MAX_RGB)
+    {
+        red = r;
+    } else{
+        cout << "Error: Red value must be between " 
+             << MIN_RGB << " and " << MAX_RGB << "." << endl;
+    }
+}
+
+void Color::setGreen(int r) 
+{
+    if (green >= MIN_RGB && green <= MAX_RGB)
+    {
+        green = g;
+    } else{
+        cout << "Error: Green value must be between " 
+             << MIN_RGB << " and " << MAX_RGB << "." << endl;
+    }
+}
+
+void Color::setBlue(int b) 
+{
+    if (blue >= MIN_RGB && blue <= MAX_RGB)
+    {
+        blue = b;
+    } else{
+        cout << "Error: Blue value must be between " 
+             << MIN_RGB << " and " << MAX_RGB << "." << endl;
+    }
+}
+
+//getRed() returns red value
+//arguments: none
+//returns: integer
+int Color::getRed() const
+{
+    return red;
+}
+
+//getGreen() returns green value
+//arguments: none
+//returns: integer
+int Color::getGreen() const
+{
+    return green;
+}
+
+//getBlue() returns blue value
+//arguments: none
+//returns: integer
+int Color::getBlue() const
+{
+    return blue;
+}
+
+//print() outputs RGB values
+//arguments: none
+//returns: nothing
+void Color::print() const
+{
+    cout << "RGB(" << red << ", " 
+         << green)"
+}
