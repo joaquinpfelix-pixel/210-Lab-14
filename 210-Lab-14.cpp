16de2b9a1174dc6dc8587169cc8ad6d7cdd7ec6c
JoaquinF <joaquinpfelix@gmail.com>
2026-03-01 16:38:06 -0800

    Check 1

--- 210-Lab-14.cpp
+++ 210-Lab-14.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// Constant declarations
+const int MIN_RGB = 0;
+const int MAX_RGB = 255;
 
+//Color class with the three variables
 class Color
 {
+    private:
     int red;
     int green;
     int blue;
 
+    public:
+    void setRed(int r);
+    void setGreen(int g);
+    void setBlue(int b);
+
+    int getRed() const;
+    int getGreen() const;
+    int getBlue() const;
+
+    void print() const;
 };
 
 
@@ -17,4 +32,12 @@ int main()
 
 
 
+}
+
+
+void populateColor(Color &c, int r, int g, int b)
+{
+    c.setRed();
+    c.setGreen();
+    c.setBlue();
 }
